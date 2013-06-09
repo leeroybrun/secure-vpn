6987cb9b31c482644ca08d4e70947c281e904395
Leeroy Brun <leeroy.brun@gmail.com>
2013-06-09 22:43:26 +0200

    Updated

--- synology_secure_vpn.sh
+++ synology_secure_vpn.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 ############################################
-# Make it work on Synology :
 # 
-#    1. Install Synology Bootstrap : http://forum.synology.com/wiki/index.php/How_to_Install_Bootstrap
-#    2. Install new busybox : ipkg install busybox
-#    3. 
-#
-#
 
 case "$1" in
 	start)
@@ -39,16 +33,9 @@ case "$1" in
 		iptables -A INPUT -s 192.168.1.0/24 -j ACCEPT
 		iptables -A OUTPUT -d 192.168.1.0/24 -j ACCEPT
 
-		echo "Allow access to certains ports without VPN"
-		iptables -A INPUT -i eth0 -p tcp -m multiport --dports 22,5225 -m state --state NEW,ESTABLISHED -j ACCEPT
-		iptables -A OUTPUT -o eth0 -p tcp -m multiport --sports 22,5225 -m state --state ESTABLISHED -j ACCEPT
-
 		echo "Drop anything else..."
 		iptables -A INPUT -j DROP
 		iptables -A OUTPUT -j DROP
-
-		echo ""
-		#busybox ip route add default table 100 via 192.168.1.1
 
 		echo "For testing purpose, reset iptables after 20 seconds"
 		sleep 20
