355b245f9e35f73007d3fbd8f81d6c3849db6690
Leeroy Brun <leeroy.brun@POSTE-LEEROY.batiplus.local>
2013-06-12 12:47:10 +0200

    Add server switch when cannot connect & new install script

--- install.sh
+++ install.sh
@@ -4,16 +4,45 @@ source ./config/config.sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Login conf
-echo "$VPN_USERNAME
-$VPN_PASSWORD" > ./config/login.conf
+# ------------------------------------------------
+# Generate login.conf file
+# ------------------------------------------------
+	echo "$VPN_USERNAME
+	$VPN_PASSWORD" > ./config/login.conf
 
-cat >> /etc/network/if-up.d/secure-vpn << EOF
+# ------------------------------------------------
+# Create file who will start VPN and add iptable
+# rules when network goes up.
+# Only if no "non-persistent" parameter was passed 
+# to script.
+# ------------------------------------------------
+	if [ "$1" != "non-persistent" ]; then
+		cat >> /etc/network/if-up.d/secure-vpn << EOF
 #!/bin/bash
 
 $DIR/secure-vpn.sh start
 EOF
 
-chmod +x /etc/network/if-up.d/secure-vpn
+		chmod +x /etc/network/if-up.d/secure-vpn
+	fi
+
+# ------------------------------------------------
+# Generate good paths for OpenVPN client config
+# ------------------------------------------------
+	# Get line where the fun starts
+	startLine=$(awk '$0 ~ str{print NR FS b}{b=$0}' str="### CUSTOM PATHS ###" $DIR/config/client.ovpn)
+
+	# Keep only first part of the file (remove "custom" options at the end)
+	head -n $startLine $DIR/config/client.ovpn > $DIR/config/client.ovpn.tmp
+	mv $DIR/config/client.ovpn.tmp $DIR/config/client.ovpn
+
+	# Add \n at the end of the file only if it doesn't already end in a newline
+	sed -i -e '$a\' $DIR/config/client.ovpn
+
+	# Write paths config to file
+	echo "auth-user-pass $DIR/config/login.conf" >> $DIR/config/client.ovpn
+	echo "up $DIR/scripts/vpn_up.sh" >> $DIR/config/client.ovpn
+	echo "" >> $DIR/config/client.ovpn
+	echo "### CUSTOM OPTIONS ###" >> $DIR/config/client.ovpn
 
 exit 0
--- scripts/vpndaemon.sh
+++ scripts/vpndaemon.sh
@@ -2,17 +2,83 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+currentServer=""
+retry=0
+
+# ------------------------------------------------
+# Get status of VPN connection
+# ------------------------------------------------
 function getStatus {
 	ifconfig | grep "$1" && return 1
 	return 0
 }
 
+# ------------------------------------------------
+# Get random server from config file and check if
+# it was not already the previous one
+# ------------------------------------------------
+function getRandomServer {
+	randomServer=$(shuf -n 1 $DIR/../config/servers.conf)
+
+	if [ "$randomServer" != "$currentServer" ]; then
+		currentServer=$randomServer
+		read srvName srvIp srvPort <<< $currentServer
+	else
+		getRandomServer
+	fi
+}
+
+# ------------------------------------------------
+# Write new server config to OpenVPN config file
+# ------------------------------------------------
+function writeOvpnConfig {
+	# Get line where the fun starts
+	startLine=$(awk '$0 ~ str{print NR FS b}{b=$0}' str="### CUSTOM OPTIONS ##" $DIR/config/client.ovpn)
+
+	# Keep only first part of the file (remove "custom" options at the end)
+	head -n $startLine $DIR/config/client.ovpn > $DIR/config/client.ovpn.tmp
+	mv $DIR/config/client.ovpn.tmp $DIR/config/client.ovpn
+
+	# Add \n at the end of the file only if it doesn't already end in a newline
+	sed -i -e '$a\' $DIR/config/client.ovpn
+
+	# Write server config to file
+	echo "remote $srvIp $srvPort" >> $DIR/config/client.ovpn
+	echo "ca $DIR/config/certs/$srvName.crt" >> $DIR/config/client.ovpn
+}
+
+# ------------------------------------------------
+# Infinite loop for daemon - check if VPN is up
+# and restart it if needed. If can't connect to
+# VPN server after 3 retry, load new server config.
+# ------------------------------------------------
 while :
 do
 	getStatus tun0
 	if [[ $? == 0 ]]; then
-		echo "OpenVPN not connected ! Reconnecting..."
+		date
+		echo "OpenVPN not connected !"
+
+		# Tried 3 times connecting to server ? Get new one from config.
+		if [ "$retry" -eq "3" ]; then
+			echo "Tried 3 times connecting to $srvName. Get new random server..."
+
+			getRandomServer
+
+			echo "New server : $currentServer"
+
+			echo "Write OpenVPN config file..."
+
+			writeOvpnConfig
+
+			retry=0
+		fi
+
+		echo "Connection to server..."
+
 		openvpn --config $DIR/../config/client.ovpn
+
+		retry=$[retry + 1]
 
 		# Wait 1 minute before next check
 		sleep 60
