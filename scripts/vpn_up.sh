6ea2cda84328306ced8122b044a1093513cc335f
Leeroy Brun <leeroy.brun@POSTE-LEEROY.batiplus.local>
2013-06-12 17:34:11 +0200

    Update vpn_up.sh script with config vars - all working

--- scripts/vpn_up.sh
+++ scripts/vpn_up.sh
@@ -10,6 +10,6 @@ ip route flush cache
 
 # https://forum.linode.com/viewtopic.php?p=50114&sid=b440414422596bb7dbc96cf7c9ee511f#p50114
 # Allow packets to go back to eth0 interface, as all ports are blocked, only the allowed ones will be routed
-ip rule add from 91.121.166.103 table 100
+ip rule add from $SERVER_IP table 100
 #ip route add table 100 to 91.121.166.0/24 dev eth0
-ip route add table 100 default via 91.121.166.254
+ip route add table 100 default via $WAN_GATEWAY
