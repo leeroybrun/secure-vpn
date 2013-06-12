bf833a66e5e541a7c7e2502d10b3d458518a2225
Leeroy Brun <leeroy.brun@POSTE-LEEROY.batiplus.local>
2013-06-12 12:05:05 +0200

    Update OpenVPN config vile path

--- scripts/vpndaemon.sh
+++ scripts/vpndaemon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 function getStatus {
 	ifconfig | grep "$1" && return 1
 	return 0
@@ -10,7 +12,7 @@ do
 	getStatus tun0
 	if [[ $? == 0 ]]; then
 		echo "OpenVPN not connected ! Reconnecting..."
-		openvpn --config /root/secure-vpn/config/client.ovpn
+		openvpn --config $DIR/../config/client.ovpn
 
 		# Wait 1 minute before next check
 		sleep 60
